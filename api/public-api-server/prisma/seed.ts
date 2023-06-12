68a92d583fb87faa7fbf562cb445cf5bf98e2e38
Leandro Manzanal <leomanzanal@gmail.com>
2023-06-12 09:57:21 -0300

    public api cors

--- api/public-api-server/prisma/seed.ts
+++ api/public-api-server/prisma/seed.ts
@@ -1,27 +1,38 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
   const maz = await prisma.person.upsert({
-    where: { email: 'mazito.v2@gmail.com' },
+    where: { email: "mazito.v2@gmail.com" },
     update: {},
     create: {
-      email: 'mazito.v2@gmail.com',
+      email: "mazito.v2@gmail.com",
       accountId: "0",
-      fullName: 'Mario Z',
-      state: 'PENDING'
+      fullName: "Mario Z",
+      state: "PENDING",
     },
-  })
-  console.log({ maz })
+  });
+  console.log({ maz });
+  const user = await prisma.person.upsert({
+    where: { email: "leomanzanal@gmail.com" },
+    update: {},
+    create: {
+      email: "leomanzanal@gmail.com",
+      fullName: "Manza",
+      state: "PENDING",
+      accountId: "-",
+    },
+  });
+  console.log({ user });
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
