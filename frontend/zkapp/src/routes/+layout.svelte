0826a19a2e321a0eb4340aaffa1c05583e88f8c7
Nicolas Massi <massinicolas@gmail.com>
2023-07-14 14:30:32 +0200

    homepage cards

--- frontend/zkapp/src/routes/+layout.svelte
+++ frontend/zkapp/src/routes/+layout.svelte
@@ -1,43 +1,48 @@
 <script>
-  import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { getItem } from '$lib/utility/localStorageController';
-	import { page } from '$app/stores';
-  
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import { getItem } from "$lib/utility/localStorageController";
+  import { page } from "$app/stores";
+
   // local imports ...
-  import { Styles } from 'sveltestrap';
-  import RootHeader from '@components/RootHeader.svelte';
+  import { Styles } from "sveltestrap";
+  import RootHeader from "@components/RootHeader.svelte";
 
   // Hexui
-  let isAuthenticated = getItem('access_token');
-	let currentPage = $page.url.pathname;
+  let isAuthenticated = getItem("access_token");
+  let currentPage = $page.url.pathname;
 
   onMount(async () => {
-		if (isAuthenticated && isAuthenticated !== '') {
-  			goto(currentPage);
-  		} else {
-  			goto(`/signin`);
-  		}   
+    if (isAuthenticated && isAuthenticated !== "") {
+      goto(currentPage);
+    } else {
+      goto(`/signin`);
+    }
   });
 </script>
 
+<svelte:head>
+  <title>Socialcap</title>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+  />
+</svelte:head>
+
+<div style="position:fixed;top:0;left:0;bottom:0;right:0;">
+  <slot />
+</div>
+
 <!-- MUST include here Bootstrap styling 
 -->
-<Styles />
+<!-- <Styles /> -->
 
 <!--
   MINA original not used here
 -->
 <style global>
-  @import '/css/mina-fonts.css';
+  /* @import "/css/mina-fonts.css"; */
   /* @import '/css/bootstrap.css'; */
-  @import '/css/custom.css';
+  /* @import "/css/custom.css"; */
+  @import "/css/custom.min.css";
 </style>
-
-<svelte:head>
-  <title>Socialcap</title>
-</svelte:head>
-
-<div style="position:fixed;top:0;left:0;bottom:0;right:0;">
-  <slot></slot>
-</div>
