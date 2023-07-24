14323aa35d38852f80a62dd1a1357d54218cb55d
Mario Zito <mario.zito@treme.io>
2023-07-24 15:42:28 -0300

    Join a community

--- frontend/zkapp/src/lib/apis/mutations.ts
+++ frontend/zkapp/src/lib/apis/mutations.ts
@@ -1,7 +1,11 @@
 import { apiClient, AppStatus } from "$lib/globals";
 import { monitorMINATransaction } from "./monitors";
 
-export { updateProfile, updateCommunity }
+export { 
+  updateProfile, 
+  updateCommunity, 
+  joinCommunity 
+}
 
 
 async function updateProfile(data: any): Promise<any> {
@@ -35,4 +39,25 @@ async function updateCommunity(data: any): Promise<any> {
   monitorMINATransaction(rs.data.transaction.id);
 
   return rs.data.community;
+}
+
+
+async function joinCommunity(data: any): Promise<any> {
+  AppStatus.push("Adding as member to a community ...");  
+  let { communityUid, personUid } = data;
+
+  let rs = await apiClient.mutate("join_community", {
+    communityUid: communityUid,
+    personUid: personUid
+  });
+  if (rs.error) {
+    AppStatus.error("There is some error with the data, please review !");
+    return null;
+  }  
+  AppStatus.push("Member added !");  
+  
+  AppStatus.push("Now waiting for MINA transaction to complete");  
+  monitorMINATransaction(rs.data.transaction.id);
+
+  return rs.data.member;
 }
