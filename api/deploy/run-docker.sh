5b139be33929e6e80ade5fa43589f7531d6648f6
Mario Zito <mario.zito@treme.io>
2023-08-08 00:14:18 -0300

    Run docker as www-data and not as root

--- api/deploy/run-docker.sh
+++ api/deploy/run-docker.sh
@@ -3,5 +3,10 @@
 # will run the Socialcap API in host port 3080 
 docker -l debug run -d --restart=always --name scapi \
   --net=host \
+  --user $(id -u www-data):$(id -g www-data) \
+  --env USER=www-data \
+  --env GROUP=www-data \
+  --env UID=$(id -u www-data) \
+  --env GID=$(id -g www-data) \
   -v /etc/localtime:/etc/localtime:ro \
   socialcap/api:run
