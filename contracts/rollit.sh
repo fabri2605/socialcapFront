b5d95ee7215d51f378a443f8427a75861ad86851
Mario Zito <mario.zito@treme.io>
2023-11-02 16:43:51 -0300

    Delete image (link) from provable community

--- contracts/rollit.sh
+++ contracts/rollit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # $1 is Contract name = Communities, Claimings, Electors, Socialcap
+cat logs/rollit.log >> logs/rollits-history.log
 
 npm run build
 
-node build/src/tests/voting-rollup-tests.js Berkeley proofsEnabled >> logs/rollit.log
-
+node build/src/tests/voting-rollup-tests.js Berkeley proofsEnabled > logs/rollit.log
