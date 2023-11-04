c778a96752fcd9ade7230195a6998fecc2fbd6b8
Mario Zito <mario.zito@treme.io>
2023-11-04 20:39:10 -0300

    Fix: avoid reload COI cause hangs the browser in incognito mode

--- frontend/zkapp/static/coi-serviceworker.js
+++ frontend/zkapp/static/coi-serviceworker.js
@@ -100,7 +100,7 @@ if (typeof window === 'undefined') {
 
                     registration.addEventListener("updatefound", () => {
                         !coi.quiet && console.log("Reloading page to make use of updated COOP/COEP Service Worker.");
-                        coi.doReload();
+                        //coi.doReload();
                     });
 
                     // If the registration is active, but it's not controlling the page
