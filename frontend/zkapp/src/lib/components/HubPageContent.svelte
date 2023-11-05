dafd68830d4649fd61fed966bf7d0e049f39903d
Mario Zito <mario.zito@treme.io>
2023-11-04 21:33:12 -0300

    Improve contrats of container background, was too light

--- frontend/zkapp/src/lib/components/HubPageContent.svelte
+++ frontend/zkapp/src/lib/components/HubPageContent.svelte
@@ -1,4 +1,5 @@
-<div class="--bg-light" style="position:fixed;margin:0;top:120px;left:0;bottom:0;right:0;border:0px dotted red;overflow:auto;">
+<div class="bg-container" 
+  style="position:fixed;margin:0;top:120px;left:0;bottom:0;right:0;border:0px dotted red;overflow:auto;">
   <!-- <section 
     class='bg-image z-n1 position-absolute opacity-50'  
     style="background-image: url('/img/svg/socialcap-bg-signin.svg');height: 100vh; width: 100vw; backgroundRepeat: no-repeat; backgroundSize: auto;">
