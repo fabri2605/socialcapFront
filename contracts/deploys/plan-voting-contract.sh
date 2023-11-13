029b70cace0eb153529d02629061385eabf5ec8c
Mario Zito <mario.zito@treme.io>
2023-11-12 21:49:22 -0300

    Created two contract folders: batch-voting (new) and claim-voting (old)

--- contracts/deploys/plan-voting-contract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# $1 is Contract name = Communities, Claimings, Electors, Socialcap, PlanVoting
-export NAME=PlanVoting
-
-npm run build
-
-node build/src/deploy/deploy-zkapp.js Berkeley proofsEnabled $NAME 
-#> logs/$NAME.deploy.log
