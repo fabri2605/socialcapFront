a28df70dde96965cbff7b9234a341c2a81ecc751
manza <leomanzanal@gmail.com>
2023-12-06 10:45:55 -0300

    improve community page

--- frontend/zkapp/src/routes/(detail)/community/[uid]/+page.js
+++ frontend/zkapp/src/routes/(detail)/community/[uid]/+page.js
@@ -1,11 +1,16 @@
 
 import { error } from '@sveltejs/kit';
-import { getCommunity } from '@apis/queries';
-
+import { getCommunity, getMyClaims, getMyCredentials } from '@apis/queries';
+import { APPROVED } from "@models/states";
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
     if (params.uid !== "") {
         let obj = await getCommunity(params.uid);
+        const claimed = await getMyClaims(params);
+        const credentials = await getMyCredentials({...params, communityUid: params.uid});
+        obj.claimed = claimed;
+        obj.credentials = credentials;
+        obj.approvedClaims = obj.claims.filter((c) => c.state == APPROVED);
         obj.claimables = []; // await getClaimables(params.uid)
         return JSON.parse(JSON.stringify(obj));
     }
---
+++ frontend/zkapp/src/routes/(detail)/community/[uid]/MemberItem.svelte
@@ -0,0 +1,19 @@
+
+<div class="row fs-sm mx-0 mb-2 ps-4 py-1 --border-bottom border-0">
+  <span class="col-7 text-start">
+    <b class="fs-nm">{p.fullName}</b>
+    <br><span class="fs-sm">{p.uid}</span>
+  </span>
+</div>
+
+<script>
+  
+  export let p; 
+
+  let stateColors = {
+    "APPLIED": "warning",
+    "ACCEPTED": "success",
+    "DENIED": "danger"
+  }
+
+</script>
